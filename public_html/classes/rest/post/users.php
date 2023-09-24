cbd5ee75e545ce58660421c07c4de626fb582e67
artemsuchkov <artemsuchkov@gmail.com>
2023-09-24 23:04:41 +0300

    Добавил класс connect

--- public_html/classes/rest/post/users.php
+++ public_html/classes/rest/post/users.php
@@ -2,6 +2,7 @@
 
 namespace classes\rest\post;
 use \classes\rest\Common AS Common;
+use \classes\db\Connect as Connect;
 
 class Users {
 
@@ -14,10 +15,25 @@ class Users {
             ], 400);
         }
 
+        $conn = Connect::getInstance();
+        //print_r($conn->conn);
+
+        $result = mysqli_query($conn->conn," SELECT * FROM `users` WHERE id='1' ");
+        $user = mysqli_fetch_array($result);
+        //print_r($user);
+
+        if($user["name"])
+
+            Common::send_response([
+                'status' => 'success',
+                'message' => 'User #1 post'.' '.$user["name"].' '.$data["last_name"].' ' ,
+            ], 200);
+        
         Common::send_response([
-            'status' => 'success',
-            'message' => 'User #1 post'.' '.$endpoint[0].' '.$data["FirstName"].' '.$data["LastName"],
-        ], 200);
+            'status' => 'failed',
+            'message' => 'Error. No user!',
+        ], 400);
+        
     }
 
 }
--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,57 @@
 
 spl_autoload_register();
 
+
 use \classes\rest\Common as Common;
 use \classes\rest\get\MethodGet as MethodGet;
 use \classes\rest\post\MethodPost as MethodPost;
 use \classes\rest\put\MethodPut as MethodPut;
 use \classes\rest\delete\MethodDelete as MethodDelete;
+
+/*
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = mysqli_connect($host, $user, $pass, "test_db");
+
+mysqli_query($conn, "SET NAMES 'utf8'");
+$result = mysqli_query($conn," SELECT * FROM `users` ");
+ 
+ 
+while($page = mysqli_fetch_array($result)) {
+	echo $page["name"];
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 switch (Common::get_method()) {
 
